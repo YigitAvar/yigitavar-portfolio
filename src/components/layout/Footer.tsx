f112c65a7a0bec6a369dd35fb68092617649aa1b
Yiğit Avar <167037822+YigitAvar@users.noreply.github.com>
2026-05-08 05:15:56 +0300

    Footer Linkedin link fix

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -21,7 +21,7 @@ function Footer() {
             <FaGithub size={18} />
           </a>
 
-          <a href="https://www.linkedin.com/in/yigit-avar" target="_blank" rel="noreferrer" aria-label="LinkedIn">
+          <a href="https://www.linkedin.com/in/yiğit-avar-567a38299" target="_blank" rel="noreferrer" aria-label="LinkedIn">
             <FaLinkedinIn size={18} />
           </a>
 
